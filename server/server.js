0f6971ff7ad1bc03a96dbe33521631a9b3023ab7
mhmdshorafa <mhmd_shorafa@hotmail.com>
2018-04-21 11:19:02 +0300

    fix port bug to deploy

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const server = require('http').Server(app); //eslint-disable-line
 
 app.use(express.static('public'));
 
-server.listen(4000, () => {
+server.listen(process.env.PORT || 4000, () => {
   console.log('Our server is running on http://localhost:' + 4000);
 });
